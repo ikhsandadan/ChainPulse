c9c1a4cd93fe2745eb86b24a9bf0e70402681295
Nashki <116878888+ikhsandadan@users.noreply.github.com>
2025-04-11 19:28:50 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,13 +39,13 @@ export default function Homepage() {
                                 id="txHash"
                                 value={txHash}
                                 onChange={(e) => setTxHash(e.target.value)}
-                                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#0c3c6f]"
                                 placeholder="0x..."
                             />
                         </div>
                         <button
                             type="submit"
-                            className="self-center max-h-12 bg-blue-600 text-white py-1 px-2 mt-3 ml-2 rounded-lg hover:bg-blue-700 transition-colors cursor-pointer"
+                            className="self-center max-h-12 bg-[#0c3c6f] text-white py-1 px-2 mt-3 ml-2 rounded-lg hover:bg-[#56b9d5] transition-colors cursor-pointer"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 64 64" className='fill-white'>
                             <path d="M 27 9 C 17.075 9 9 17.075 9 27 C 9 36.925 17.075 45 27 45 C 31.129213 45 34.9263 43.587367 37.966797 41.240234 L 51.048828 54.322266 C 51.952828 55.226266 53.418266 55.226266 54.322266 54.322266 C 55.226266 53.418266 55.226266 51.952828 54.322266 51.048828 L 41.240234 37.966797 C 43.587367 34.9263 45 31.129213 45 27 C 45 17.075 36.925 9 27 9 z M 27 13 C 34.719 13 41 19.281 41 27 C 41 34.719 34.719 41 27 41 C 19.281 41 13 34.719 13 27 C 13 19.281 19.281 13 27 13 z"></path>
